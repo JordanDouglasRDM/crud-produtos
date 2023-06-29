28e28ef4e320a77c002f6b85218f79ae822dc422
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2023-06-29 00:51:09 -0300

    Atualização de repositorio

--- pages/index.php
+++ pages/index.php
@@ -1,36 +1,57 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto CRUD</title>
-
-    <link rel="stylesheet" href="../public/css/inicio.css">
+<link rel="stylesheet" href="../public/css/inicio.css">
 </head>
 <body>
+    <style>
+        input {
+            width: 350px;
+        }
+    </style>
+
+    <script src="https://kit.fontawesome.com/9f50e2463f.js" crossorigin="anonymous"></script>
     <h1>Cadastro</h1>
-    <table id="tabela1">
-        <tr>
-          <th>Nome do Produto</th>
-          <td>sdjajdajd</td>
-        </tr>
-        <tr>
-          <th>Quantidade</th>
-          <td>sdadshd</td>
-        </tr>
-        <tr>
-            <th>Código do Produto</th>
-            <td>dhdhhs</td>
-        </tr>
-    </table>
-      
+    <form action="" method="post">
+
+        <table id="tabela1">
+            <tr>
+                <th>
+                    <label for="prod">Produto</label>
+                </th>
+                <td>    
+                    <input type="text" name="prod" id="prod">        
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <label for="quant">Quantidade</label>
+                </th>
+                <td>
+                    <input type="number" name="quant" id="quant"> 
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <label for="quant">Valor do Produto</label>
+                </th>
+                <td>
+                    <input type="number" step="0.01" min="0" placeholder="R$ 0,00" />
+                </td>
+            </tr>
+        </table>
+    </form>
+            
 
     <table id="tabela2">
         <tr>
             <th>Produto</th>
             <th>Quantidade</th>
             <th>Código</th>
-            <th>Ações</th>
+            <th id="col-acoes" ></th>
         </tr>
         <tr>
             <td>dhdhhs</td>
@@ -90,7 +111,12 @@
             <td>dhdhhs</td>
             <td>dhdhhs</td>
             <td>dhdhhs</td>
-            <td>dhdhhs</td>
+        <td>
+                <a href="./acoes.php" >
+                    <i class="fa-solid fa-trash" style="color: #ff0000;"></i>
+                </a>
+            </td>
+        
         </tr>
     </table>
 </body>
