edd7cb780063c04b7bd0c85fd0f02968db70d26d
Jordan Douglas <jordandouglas8515@gmail.com>
2023-06-28 22:57:48 -0300

    Meu primeiro commit
    Iniciando o projeto

---
+++ pages/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto CRUD</title>
+
+    <link rel="stylesheet" href="../public/css/inicio.css">
+</head>
+<body>
+    <h1>Cadastro</h1>
+    <table id="tabela1">
+        <tr>
+          <th>Nome do Produto</th>
+          <td>sdjajdajd</td>
+        </tr>
+        <tr>
+          <th>Quantidade</th>
+          <td>sdadshd</td>
+        </tr>
+        <tr>
+            <th>Código do Produto</th>
+            <td>dhdhhs</td>
+        </tr>
+    </table>
+      
+
+    <table id="tabela2">
+        <tr>
+            <th>Produto</th>
+            <th>Quantidade</th>
+            <th>Código</th>
+            <th>Ações</th>
+        </tr>
+        <tr>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+        </tr>
+        <tr>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+        </tr>
+        <tr>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+        </tr>
+        <tr>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+        </tr>
+        <tr>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+        </tr>
+        <tr>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+        </tr>
+        <tr>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+        </tr>
+        <tr>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+        </tr>
+        <tr>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+        </tr>
+        <tr>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+            <td>dhdhhs</td>
+        </tr>
+    </table>
+</body>
+</html>
