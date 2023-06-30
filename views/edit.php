02fc46aa0f0031d6ad25720d3fd5a404c9df0413
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2023-06-30 01:49:02 -0300

    Melhora do CSS da página principal e da página de alterações

--- views/edit.php
+++ views/edit.php
@@ -12,14 +12,14 @@
 </head>
 <body>
 <a href="/">
-    <i class="fa-solid fa-circle-left" style="color: #ffffff;"></i>
+    <img src="img/IconeSaida.png">
 </a>
 <form action="/update" method="post">
     <input type="hidden" value="<?= $produto->id; ?>">
     <label for="nome">Produto</label><br><br>
-    <input type="text" name="nome" id="nome" value="<?= $produto->nome; ?>"><br><br>
-    <label for="nome">Preco</label><br><br>
-    <input name="preco" id="preco" type="number" step="0.01" min="0" value="<?= $produto->preco; ?>"><br><br><br>
+    <input type="text" name="nome" id="nome" placeholder="Nome do Produto" value="<?= $produto->nome; ?>"><br><br>
+    <label id="preco1" for="nome">Preço</label><br><br>
+    <input name="preco" id="preco" type="number" step="0.01" min="0" placeholder="R$ 0,00" value="<?= $produto->preco; ?>"><br><br><br>
     <input id="button2" type="submit" value="Atualizar">
 </form>
 </body>
--- views/initial.php
+++ views/initial.php
@@ -7,22 +7,15 @@
 <link rel="stylesheet" href="/css/inicio.css">
 </head>
 <body>
-<style>
-    .transparent-input {
-        background-color: transparent;
-        border: none;
-        outline: none;
-    }
-</style>
     <script src="https://kit.fontawesome.com/9f50e2463f.js" crossorigin="anonymous"></script>
-    <h1>Cadastro</h1>
+    <h1><i id="carrinho" class="fa-solid fa-cart-shopping" style="color: #ffffff;"></i>Carrinho de Compras</h1>
     <form action="/novo" method="POST">
         <table id="tabela1">
             <tr>
                 <th>
                     <label for="nome">Produto</label>
                 </th>
-                <td>    
+                <td class="ReduzirColuna">    
                     <input type="text" name="nome" id="nome">
                 </td>
             </tr>
@@ -30,7 +23,7 @@
                 <th>
                     <label for="quant">Quantidade</label>
                 </th>
-                <td>
+                <td class="ReduzirColuna">
                     <input type="number" name="quant" id="quant"> 
                 </td>
             </tr>
@@ -38,19 +31,20 @@
                 <th>
                     <label for="preco">Valor do Produto</label>
                 </th>
-                <td>
+                <td class="ReduzirColuna">
                     <input name="preco" id="preco" type="number" step="0.01" min="0" placeholder="R$ 0,00" />
                 </td>
             </tr>
         </table>
-        <input type="submit" value="Incluir">
+        <input id="button1" type="submit" value="Incluir">
     </form>
+
     <table id="tabela2">
         <tr>
             <th>ID</th>
             <th>Produto</th>
             <th>Preco</th>
-            <th id="col-acoes"></th>
+            
         </tr>
         <?php
         /** @var \Crud\PDO\Entity\Produto[] $produtoList */
@@ -59,7 +53,7 @@
             <td><?= $produto->id; ?></td>
             <td><?= $produto->nome; ?></td>
             <td><?= $produto->preco; ?></td>
-            <td>
+            <td class="icones">
                 <a href="/form?id=<?= $produto->id; ?>">
                     <i class="fa-solid fa-pen-to-square" style="color: #9c9c9c;"></i>
                 </a>
@@ -70,5 +64,7 @@
         </tr>
         <?php endforeach; ?>
     </table>
+
+    <h1 id="total">Total da Compra: </h1>
 </body>
 </html>
